489f2177814cd6528321028bf91547367ae510fe
Willem Basson <wlmbasson@gmail.com>
2023-04-10 14:27:19 +0200

    Increase async def test_accept_proof_request_oob_v1 timeout

--- app/tests/e2e/test_oob.py
+++ app/tests/e2e/test_oob.py
@@ -1,7 +1,7 @@
 from aries_cloudcontroller import AcaPyClient
 import pytest
 import time
-# import asyncio
+import asyncio
 from assertpy import assert_that
 from httpx import AsyncClient
 
@@ -12,11 +12,11 @@ from app.tests.util.webhooks import (
 
 # When using the default module scoped fixture in ./conftest.py test_oob_connect_via_public_did fails
 # Setting temporary event_loop override for tests in this file.
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.mark.asyncio
